787937afc52f3b3e8bc744b8c5562e691e77256c
DiegoVanegas03 <gamesonfn@gmail.com>
2026-03-18 00:16:27 -0600

    feat: Introduce global toast notifications, enhance login and register forms with autocomplete and loading states, and update authentication guards and button component for signal compatibility.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@core/services/auth/auth.service';
+import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
   standalone: true,
+  imports: [RouterOutlet, ToastComponent],
+  templateUrl: './app.html',
 })
-export class App {
+export class App implements OnInit {
+  authService = inject(AuthService);
   protected readonly title = signal('eduno');
+
+  ngOnInit(): void {
+    this.authService.checkSession().subscribe();
+  }
 }
